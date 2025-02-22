3cd4be8ad93a2a529b78c3518f34864acc71bc6c
Unmesh Ghosh <bubunghosh1214@gmail.com>
2025-02-22 12:44:32 +0530

    intial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+
+import toast, { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/js/swiper-bundle.min.js"></script>
+      <script src="assets/js/countto.js"></script>
+      <script src="assets/js/simpleParallax.min.js"></script>
+      <script src="assets/js/gsap.js"></script>
+      <script src="assets/js/SplitText.js"></script>
+      <script src="assets/js/wow.min.js"></script>
+      <script src="assets/js/ScrollTrigger.js"></script>
+      <script src="assets/js/gsap-animation.js"></script>
+      <script src="assets/js/tsparticles.min.js"></script>
+      <script src="assets/js/main.js"></script>
+    </>
+  );
 }
